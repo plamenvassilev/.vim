f2a2ef1e4a15a1fe95498a0034697ed36a99d80d
Plamen Vassilev <vassilev.plamen@gmail.com>
2021-08-29 10:43:02 -0400

    Adds changes

--- setup.sh
+++ setup.sh
@@ -1,4 +1,7 @@
 #!/usr/bin/env bash
 
-git clone https://github.com/fatih/vim-go.git
-~/.vim/pack/plugins/start/vim-go
+if [ ! -d "~/.vim/pack/plugins/start" ]; then
+	mkdir -p "~/.vim/pack/plugins/start"
+fi
+
+git clone https://github.com/fatih/vim-go.git ~/.vim/pack/plugins/start/vim-go
